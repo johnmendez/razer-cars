c0e5973f23fa3c67b150220900b19b293422ed02
johnmendez <john.a.mendez2012@gmail.com>
2017-04-28 15:35:20 -0500

    Cars quick fix

--- app/router.js
+++ app/router.js
@@ -6,11 +6,20 @@ const Router = Ember.Router.extend({
   rootURL: config.rootURL
 });
 
+// Update your routes to load/save data using Ember data instead of bespoke fetch requests
+
 Router.map(function() {
+  // manufacturer.index
   this.route('manufacturer', { path: '/manufacturers'}, function() {
+    // manufacturer.new
     this.route('new');
+    // Create an edit route
+    //  Link to edit route
+    // Save model changes on form submit and redirect to manufacturer.car
     this.route('edit', { path: '/:brand_id/edit' });
+    // manufacturer.car
     this.route('car', { path: '/:id' }, function() {
+      // manufacturer.car.new
       this.route('new');
     });
   });
